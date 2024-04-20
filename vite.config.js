7a89304c8140cf8c884bbc2fa255de529d8045a7
bmancilla23 <bmancilla119@gmail.com>
2024-04-19 20:38:16 -0500

    chore: alias configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': new URL('./src', import.meta.url).pathname,
+    },
+  },
 });
-
